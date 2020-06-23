2386750a208fafafb982eb7fc9968713148d1409
jmtoniolo <55120550+jmtoniolo@users.noreply.github.com>
2020-06-23 09:34:26 -0400

    Initial commit

---
+++ spec/which-function-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import WhichFunctionView from '../lib/which-function-view';
+
+describe('WhichFunctionView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
